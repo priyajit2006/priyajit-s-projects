c97623ec3520b20d5e1a38bee175cee22f2f6a73
priyajit2006 <priyajit329@gmail.com>
2025-09-20 22:27:27 +0530

    our first version

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int start,end;
+    printf("enter the value of starting no -");
+    scanf("%d",&start);
+    printf("enter the end value -");
+    scanf("%d",&end);
+    int i = start;
+    while(i<=end)
+    { if(i%2 !=0) 
+    {
+        printf("%d\n",i);
+    }
+        i++;
+    }
+    //testing
+    return 0;
+}
