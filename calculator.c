87a6c86b4f50b3fd8237068e33e2b3784eadef96
priyajit2006 <priyajit329@gmail.com>
2025-09-22 00:36:23 +0530

    version2.0

--- calculator.c
+++ calculator.c
@@ -1,31 +1,51 @@
 #include<stdio.h>
 int main(){
-    int first,second, n, result;
-    printf("enter the 1st value-");
+    int first,second,result;
+    char op;
+    printf("enter the 1st value -");
     scanf("%d",&first);
     printf("enter the second value -");
     scanf("%d",&second);
     printf("please choose oporetors -\n");
     printf(" 1. +\n 2. -\n 3. *\n 4. /\n");
-    scanf("%d",&n);
-        if(n == 1) {
+    scanf(" %c",&op);
+        if (op=='+')
+        {
             result = first+second;
-            printf("%d\n",result);
+              printf("%d\n",result);
         }
-        else if (n==2){
+        else if (op=='-')
+        {
+          result = first - second;
+            printf(" result - %d\n", result);
+        }
+        else if (op=='/'){
+          result = first / second;
+            printf(" result - %d\n", result);
+        }
+        else if (op=='*'){
+          result = first * second;
+            printf(" result - %d\n", result);
+        }
+        else if(op == '1') { 
+            result = first+second; 
+            printf("%d\n",result);
+        
+        }
+        else if (op=='2'){
              result = first - second;
-            printf("%d\n", result);
+            printf(" result - %d\n", result);
         }
-        else if (n==3){
+        else if (op=='3'){
             result = first * second;
-            printf("%d\n",result);
+            printf(" result - %d\n",result);
         }
-        else if (n==4){
+        else if (op=='4'){
             result = first / second;
-            printf("%d\n",result);
+            printf(" result - %d\n",result);
         }
          else {
-          printf("error");
+          printf("error\n");
          }
 
 return 0;
